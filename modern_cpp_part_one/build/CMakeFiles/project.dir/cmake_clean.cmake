675144832407a5840aaef3ffc9c38fcfbb59809b
Mohamed Amer <mohamed.amer.elkielani@gmail.com>
2022-09-10 20:34:16 +0200

    Adding simple Cmake script

---
+++ modern_cpp_part_one/build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/range_for_loop.cc.o"
+  "CMakeFiles/project.dir/range_for_loop.cc.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
